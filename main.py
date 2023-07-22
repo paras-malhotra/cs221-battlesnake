73d41315067ffa450205d4f17d9fb96218a714cd
Pablo Rodriguez Bertorello <pablo@1now.ai>
2023-07-22 10:58:10 -0700

    fixed main for ngrok

--- main.py
+++ main.py
@@ -56,9 +56,10 @@ def identify_server(response):
 
 
 if __name__ == "__main__":
+    ### NGROK
+    snake_app.run()
+    ### LOCAL
     # This is used when running locally only. When deploying to Google App
     # Engine, a webserver process such as Gunicorn will serve the app. You
     # can configure startup instructions by adding `entrypoint` to app.yaml.
-    snake_app.run(host="127.0.0.1", port=8080, debug=True)
-# [END gae_python3_app]
-# [END gae_python38_app]
+    # snake_app.run(host="127.0.0.1", port=8080, debug=True)
