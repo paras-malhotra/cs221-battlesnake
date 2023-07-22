c1da1cba379d1e98cc15886373b53cdf8f1c25b8
Pablo Rodriguez Bertorello <pablo@1now.ai>
2023-07-22 16:53:14 -0700

    method not allowed

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from snake_api import SnakeBrain
 
 ### app
 snake_app = Flask(__name__)
-snake_api = SnakeBrain(snake_app)
+snake_brain = SnakeBrain(snake_app)
 
 ### run 
 ## NGROK: https://www.youtube.com/watch?v=wBCEDCiQh3Q
@@ -23,32 +23,32 @@ def hello():
     Returns:
         A string with the words 'Hello World!'.
     """
-    return snake_api.info()
+    return snake_brain.info()
     # return "Hello World!"
 
 @snake_app.get("/args")
 def args():
     # game_state = request.get_json()
-    # snake_api.end(game_state)
+    # snake_brain.end(game_state)
     # return jsonify(request.get_json())
     return request.args
 
 @snake_app.post("/start")
 def on_start():
     game_state = request.get_json()
-    snake_api.start(game_state)
+    snake_brain.start(game_state)
     return "ok"
 
 @snake_app.post("/move")
 def on_move():
     game_state = request.get_json()
     snake_name = request.args["snake_name"]
-    return snake_api.move(game_state, snake_name)
+    return snake_brain.move(game_state, snake_name)
 
 @snake_app.post("/end")
 def on_end():
     game_state = request.get_json()
-    snake_api.end(game_state)
+    snake_brain.end(game_state)
     return "ok"
 
 @snake_app.after_request
