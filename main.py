bb3f5ce761d263e4f9b3410c70c76c4f339c547e
Pablo Rodriguez Bertorello <pablo@1now.ai>
2023-07-22 12:28:23 -0700

    logging

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from flask import Flask, request
 from flask_ngrok import run_with_ngrok
+from flask import has_request_context
+from flask.logging import default_handler
 import logging
 
 from snake_api import SnakeBrain
@@ -56,6 +58,21 @@ def identify_server(response):
     )
     return response
 
+class RequestFormatter(logging.Formatter):
+    def format(self, record):
+        if has_request_context():
+            record.url = request.url
+            record.remote_addr = request.remote_addr
+        else:
+            record.url = None
+            record.remote_addr = None
+
+        return super().format(record)
+formatter = RequestFormatter(
+    '[%(asctime)s] %(remote_addr)s requested %(url)s\n'
+    '%(levelname)s in %(module)s: %(message)s'
+)
+default_handler.setFormatter(formatter)
 
 if __name__ == "__main__":
     ### NGROK
